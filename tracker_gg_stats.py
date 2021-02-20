098cee6e4e92fddc32ab8ae549a61eab86baf7df
Terje Espedal <terje.m.espedal@gmail.com>
2021-02-20 12:13:56 +0100

    Added storing and checking last kills so multiple sms are not sent for one event

--- tracker_gg_stats.py
+++ tracker_gg_stats.py
@@ -1,4 +1,5 @@
 import os
+import json
 from datetime import datetime
 from pprint import PrettyPrinter
 
@@ -9,15 +10,17 @@ from dotenv import load_dotenv
 
 from main import send_message
 
+# Initialize scheduler
 scheduler = BlockingScheduler()
 load_dotenv()
 
 API_KEY = os.getenv("GG_TRACKERS_API_KEY")
 PHONES = str.split(os.getenv("PHONES"), ",")
-
 BASE_URL = "https://public-api.tracker.gg/apex/v1/standard/profile/"
 HEADERS = {"TRN-Api-Key": API_KEY}
 
+with open("last_kills.json") as f:
+    last_kills = json.load(f)
 
 def get_player_stats(platform, profile_name, segment_type=None):
     """Gets stats for a given platform (PC, Xbox, PSN) for a given player profile name
@@ -64,21 +67,29 @@ def get_player_stats(platform, profile_name, segment_type=None):
 
 def check_100_kills(player_stats, legend_to_check):
     legends = player_stats["legend_data"]
+    player_name = player_stats["profile_name"]
 
     for legend in legends:
         legend_name = legend["legend"].lower()
         if legend_name == legend_to_check.lower():
             kills = legend.get("stats").get("Kills").replace(",", "")
-            print(legend_name, kills)
-            if int(kills) % 100 == 0:
-                return kills
+            if last_kills[player_name] == kills:
+                print(f"No new kills for {player_name}")
+                break
+            else:
+                last_kills[player_name] = kills
+                with open("last_kills.json", "a") as f:
+                    json.dump(last_kills, f)
+                print(legend_name, kills)
+                if int(kills) % 50 == 0:
+                    return kills
     return False
 
 
 def check_players():
     magga = get_player_stats(2, "magnusnyquist")
     chilhoss = get_player_stats(2, "chilhoss")
-    helibent = get_player_stats(2, "helibent")
+    helibent = get_player_stats(2, "heli_bent")
 
     magga_100 = check_100_kills(magga, "gibraltar")
     chilhoss_100 = check_100_kills(chilhoss, "horizon")
@@ -93,7 +104,7 @@ def send_sms_100():
     player_kills = {
         "magga": players[0],
         "chilhoss": players[1],
-        # "chilhoss": 500,
+        #"chilhoss": 500,
         "helibent": players[2],
     }
 
@@ -105,5 +116,5 @@ def send_sms_100():
     print("Checked stats", datetime.now())
 
 
-# send_sms_100()
+#send_sms_100()
 scheduler.start()
